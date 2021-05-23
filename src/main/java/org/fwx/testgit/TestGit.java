3a7e3afffe99791ff485d09668de9bee870ea3c8
fwx <fengwenxiang@buaa.edu.cn>
2021-05-23 22:16:25 +0800

    v2

--- src/main/java/org/fwx/testgit/TestGit.java
+++ src/main/java/org/fwx/testgit/TestGit.java
@@ -3,5 +3,7 @@ package org.fwx.testgit;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello word!");
+
+        System.out.println("test v2");
     }
 }
